4b8aa035771757ac045a56d2bc110181f942565c
Yuval Garti <yuvalgarti@gmail.com>
2021-09-26 18:25:07 +0300

    Added mention functionality

--- nikud_action.py
+++ nikud_action.py
@@ -5,11 +5,13 @@ import dicta_utils
 
 
 class NikudAction(MentionAction):
-    def __init__(self, api):
+    def __init__(self, api, is_production):
         self.api = api
+        self.is_production = is_production
 
     def run(self, mention):
         comment = self.api.get_status(mention.in_reply_to_status_id)
         if is_process_tweet_needed(mention):
-            status = dicta_utils.get_dicta_nikud(comment.text) + '\n\n' + 'מקור: ' + '@' + comment.user.screen_name
-            self.api.update_status(status=status, in_reply_to_status_id=mention.id)
+            status = dicta_utils.get_dicta_tweet_text(comment.text, comment.user.screen_name)
+            if self.is_production:
+                self.api.update_status(status=status, in_reply_to_status_id=mention.id)
